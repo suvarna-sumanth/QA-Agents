979ecf3e0ade473d5db5d2669854b6e45b101fc8
Sumanth Suvarna <sumanth@instaread.com>
2026-03-10 06:11:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9700781099-55302",
+  "appId": "1:45902686437:web:61d60f2885f0dc64b8cc0c",
+  "apiKey": "AIzaSyD_LYK5B1eBkljSIjMj1GIq9ZjTcKFNiw4",
+  "authDomain": "studio-9700781099-55302.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "45902686437"
+};
